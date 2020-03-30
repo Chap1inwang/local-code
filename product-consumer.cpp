b91b78957ce1761ac905574475be10ced12bee2c
Chap1inwang <1148818926@qq.com>
2020-03-31 00:44:23 +0800

    生产者消费者demo

---
+++ product-consumer.cpp
@@ -0,0 +1,67 @@
+#include<queue>
+#include<iostream>
+#include<thread>
+#include<cstdlib>
+#include<ctime>
+#include<mutex>
+#include<condition_variable>
+#include<unistd.h>
+
+using namespace std;
+queue<int> buffer;
+mutex mutex_;
+condition_variable cv;
+bool quit=false;
+
+int main(){
+	srand((int)time(0));
+	vector<thread> productor_threads;
+	for(int i=0;i<5;++i){
+		productor_threads.emplace_back(
+				[i](){
+					while(quit==false){
+						unique_lock<mutex> lock(mutex_);
+						if(buffer.size()<300){
+							int num=rand()%100;
+							buffer.push(num);
+							cout<<"productor_threads "<<i<<": "<<num<<endl;
+						}
+						else{
+							cout<<"buffer full"<<endl;
+							cv.notify_all();
+							cv.wait(lock);
+						}
+					}
+				}
+		);
+	}
+	vector<thread> consumer_threads;
+	for(int i=0;i<3;++i){
+		consumer_threads.emplace_back(
+				[i](){
+					while(quit==false){
+						unique_lock<mutex> lock(mutex_);
+						if(buffer.size()>0){
+							cout<<"consumer_threads "<<i<<": "<<buffer.front()<<endl;
+							buffer.pop();
+						}
+						else{
+							cout<<"buffer empty"<<endl;
+							cv.notify_all();
+							cv.wait(lock);
+						}
+					}
+				}
+		);
+	}
+	sleep(1);
+	quit=true;
+	cv.notify_all();
+	for(auto& t:productor_threads){
+		t.join();
+	}
+	for(auto& t:consumer_threads){
+		t.join();
+	}
+	return 0;
+}
